5ea9273eb76d8ee07e603037cadc3e3c6c429ed0
Trent Ewald <business@ubermc.net>
2019-11-28 20:42:43 -0800

    PR https://github.com/opentok/opentok-react-native/pull/262 Happens on getting sent a coloring sheet and then ending call in that sheet and then reloading simulator.

--- src/OTSession.js
+++ src/OTSession.js
@@ -90,7 +90,6 @@ export default class OTSession extends Component {
     OT.disconnectSession(this.props.sessionId, (disconnectError) => {
       if (disconnectError) {
         this.otrnEventHandler(disconnectError);
-      } else {
         const events = sanitizeSessionEvents(this.props.sessionId, this.props.eventHandlers);
         removeNativeEvents(events);
       }
